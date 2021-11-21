bf028c38fa4e9dbef94eea7bf4367389152af0ea
Mary Wood <89048287+Mary-Wood@users.noreply.github.com>
2021-11-21 15:42:14 -0500

    Update plots.js

--- js/plots.js
+++ js/plots.js
@@ -1,7 +1,7 @@
 function init() {
     var selector = d3.select("#selDataset");
   
-    d3.json("../samples.json").then((data) => {
+    d3.json("../Beef-flavored-bacteria/samples.json").then((data) => {
       console.log(data);
       var sampleNames = data.names;
       sampleNames.forEach((sample) => {
